ab7566ded43f385f4fcecaf47885005b1fc0c058
pedrocostah <pedrocostahv@gmail.com>
2025-08-20 22:49:15 -0300

    novas alterações

--- trackfit/src/components/MotivationMessage.tsx
+++ trackfit/src/components/MotivationMessage.tsx
@@ -4,7 +4,7 @@ export function MotivationMessage() {
       <p style={{ margin: 0 }}>Crie 👋</p>
       <h1 style={{ margin: '4px 0 0' }}>Sonhe!</h1>
       <p style={{ margin: '8px 0 0', color: '#6b7280' }}>
-        Persista!
+        Persista! Nunca desista dos seus sonhos!
       </p>
       <hr style={{ marginTop: 16, borderColor: '#e5e7eb' }} />
     </header>
